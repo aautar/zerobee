7f32d15e5b042e9e9457381348fdb7dcbf4e3059
Avishkar Autar <aautar@fragmentspace.com>
2024-05-07 01:09:20 -0400

    updated docs config structure, support loading doc page into DOM

--- src/ZeroBee.mjs
+++ src/ZeroBee.mjs
@@ -15,32 +15,82 @@ const ZeroBee = function(_window) {
 
     const docHtmlMap = new Map();
 
+    const createHtmlScaffolding = function() {
+        DOMOps.appendHTML(_window.document.body, `<menu class="zb-menu"></menu>`);
+        DOMOps.appendHTML(_window.document.body, `<div class="zb-current-page"></div>`);
+    };
+
     /**
      * 
-     * @param {Object[]} _docsArr 
+     * @param {String} _slug 
      */
-    const loadDocs = function(_docsArr) {
-        const menuDOMEl = _window.document.getElementsByClassName("zb-menu")[0];
+    const loadPage = function(_slug) {
+        const pageHtml = docHtmlMap.get(_slug) || null;
 
-        // verify slugs are unique
+        // @todo wait until pageHtml not null 
 
-        for(let i=0; i<_docsArr.length; i++) {
-            docHtmlMap.set(_docsArr[i].slug, null);
-            DOMOps.appendHTML(menuDOMEl, `<li><a class="zb-menu-link" href="#${_docsArr[i].slug}">${_docsArr[i].title}</a></li>`);
-
-            mkConversionWorker.postMessage(
-                {
-                    "rootURL": rootURL + "docs/",
-                    "path": _docsArr[i].path,
-                    "slug": _docsArr[i].slug,
-                }
-            );
-
-            mkConversionWorker.onmessage = function(_msg) {
-                docHtmlMap.set(_msg.data.slug, _msg.data.html);
-                console.log(_msg.data.html);
-            };
+        if(pageHtml !== null) {
+            const pageDOMEl = _window.document.getElementsByClassName("zb-current-page")[0];
+            pageDOMEl.innerHTML = pageHtml;
         }
+    };
+
+    /**
+     * 
+     * @param {String} _slug 
+     * @param {String} _path 
+     * @param {String} _title 
+     */
+    const createDocPage = function(_slug, _path, _title) {
+        docHtmlMap.set(_slug, null);
+
+        const menuDOMEl = _window.document.getElementsByClassName("zb-menu")[0];
+        DOMOps.appendHTML(menuDOMEl, `<li><a class="zb-menu-link" href="#${_slug}">${_title}</a></li>`);
+
+        mkConversionWorker.postMessage(
+            {
+                "rootURL": rootURL + "docs/",
+                "path": _path,
+                "slug": _slug,
+            }
+        );
+
+        mkConversionWorker.onmessage = function(_msg) {
+            docHtmlMap.set(_msg.data.slug, _msg.data.html);
+            if(`#${_msg.data.slug}` === window.location.hash) {
+                loadPage(_msg.data.slug);
+            }
+        };
+    };
+
+    /**
+     * 
+     * @param {Object} _subDocs 
+     * @param {String} _parentSlug 
+     */
+    const loadDocSection = function(_subDocs, _parentSlug) {
+        for(const slug in _subDocs) {
+            let pageSlug = slug;
+            if(_parentSlug !== "") {
+                pageSlug = `${_parentSlug}/${slug}`
+            }
+
+            if(_subDocs[slug].path) {
+                createDocPage(pageSlug, _subDocs[slug].path, `¶${slug}`);
+            } else {
+                loadDocSection(_subDocs[slug], pageSlug);
+            }
+        }
+    };
+
+    /**
+     * 
+     * @param {Object} _docs
+     */
+    const loadDocs = function(_docs) {
+        // @todo verify slugs are unique
+
+        loadDocSection(_docs, "");
     };
 
     /**
@@ -61,17 +111,17 @@ const ZeroBee = function(_window) {
     };
 
     this.load = function() {
-        DOMOps.appendHTML(_window.document.body, `<menu class="zb-menu"></menu>`);
-
         PaperPlane.get("zb.json", new Map(), (_resp) => {
             loadConfig(_resp);
         });
 
         console.log(`current hash = ${window.location.hash}`);
         _window.addEventListener("hashchange", (_e) => {
-            console.log(_e);
+            loadPage(window.location.hash.substring(1));
         });
-    }
+    };
+
+    createHtmlScaffolding();
 };
 
 export { ZeroBee }
