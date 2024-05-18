d2976f6f626037262aeffc74b1f36519a4388f73
Avishkar Autar <aautar@fragmentspace.com>
2024-05-18 17:35:51 -0400

    produce outline in MarkdownConversionWorker

--- src/MarkdownConversionWorker/MarkdownConversionWorker.mjs
+++ src/MarkdownConversionWorker/MarkdownConversionWorker.mjs
@@ -1,43 +1,26 @@
 import { PaperPlane } from 'paper-plane';
 import markdownit from 'markdown-it';
+import { OutlineExtractor } from '../HTMLMarkup/OutlineExtractor.mjs';
 
 const md = new markdownit();
 
-/**
- * 
- * @param {String} _mkStr 
- * @returns {String}
- */
-const extractTitleFromMarkdownString = function(_mkStr) {
-    // grab title
-    let title = "";
-    let foundStartChar = false;
-    for(let i=0; i<_mkStr.length; i++) {
-        if(foundStartChar && _mkStr[i] === '\n') {
-            break;
-        }
-
-        if(foundStartChar === false &&_mkStr[i] === ' ' || _mkStr[i] === '\n' || _mkStr[i] === '#') {
-            continue;
-        }
-
-        foundStartChar = true;
-        title += _mkStr[i];
-    }
-
-    return title.trim();
-};
-
 self.onmessage = function(_msg) {
     PaperPlane.get(_msg.data.rootURL + _msg.data.path, new Map(), (_resp) => {
-        const title = extractTitleFromMarkdownString(_resp);
         const htmlConversionResult = md.render(_resp);
+        const outline = (new OutlineExtractor()).extract(htmlConversionResult);
+
+        let title = _msg.data.slug;
+        if(outline.length > 0) {
+            title = outline[0].title;
+        }
+
         postMessage(
             {
                 "path": _msg.data.path,
                 "slug": _msg.data.slug,
-                "title": title.length > 0 ? title : _msg.data.slug,
+                "title": title,
                 "html": htmlConversionResult,
+                "outline": outline,
             }
         );
     });
